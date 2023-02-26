528e6f52c827e601d549fd7c4e7d7961111b19c5
YuliaKovall <110860185+YuliaKovall@users.noreply.github.com>
2023-02-26 16:57:44 +0200

    renamed files, main now is actually main

--- app/src/main/java/com/example/cryptochatv3/MainActivity.java
+++ app/src/main/java/com/example/cryptochatv3/MainActivity.java
@@ -2,39 +2,13 @@ package com.example.cryptochatv3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    Animation rotateLoadingWheelAnimation;
-    ImageView imageView;
-    private static final long DELAY_TIME = 2500;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView=(ImageView)findViewById(R.id.rotatingWheel);
-        rotateLoadingWheelAnimation();
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Start the main activity after the delay time
-                Intent intent = new Intent(MainActivity.this, MainPageFirstLaunch.class);
-                startActivity(intent);
-
-                // Finish the loading activity to remove it from the back stack
-                finish();
-            }
-        }, DELAY_TIME);
-
-    }
-    private void rotateLoadingWheelAnimation() {
-        rotateLoadingWheelAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate);
-        imageView.startAnimation(rotateLoadingWheelAnimation);
     }
 }
